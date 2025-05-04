b950d9a1abee0a65be3ade83d1308518e25b9f66
nadeeja <nadeejatw@gmail.com>
2025-05-04 15:14:44 +0530

    update database connection

--- src/main/java/com/smartstock/util/ConnectionData.java
+++ src/main/java/com/smartstock/util/ConnectionData.java
@@ -2,7 +2,7 @@ package com.smartstock.util;
 
 public class ConnectionData {
 
-    public static final String DATABASE = "flexiride_db";
+    public static final String DATABASE = "SmartStock";
     public static final String USERNAME = "root";
-    public static final String PASSWORD = "123456";
+    public static final String PASSWORD = "1234";
 }
